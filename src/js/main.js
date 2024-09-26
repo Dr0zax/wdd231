1c6a3ff481ced9dbf8578c7bb6b037b1e52cc117
Dr0zax <geodashfan4@gmail.com>
2024-09-26 11:13:01 -0600

    nps 2 started

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,54 @@
-import { getParkData } from "./parkService.mjs";
+import { park, parkInfoLinks } from "./parkService.mjs";
 
-const parkData = getParkData();
+const parkData = park;
+const parkInfoLinksData = parkInfoLinks;
 
 const parkTitle = document.querySelector(".title");
 parkTitle.innerHTML = parkData.fullName;
 
-const disclaimer = document.querySelector(".disclaimer-link");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName
+function setHeaderInfo(data) {
+  const disclaimer = document.querySelector(".disclaimer-link");
+  disclaimer.href = parkData.url;
+  disclaimer.innerHTML = parkData.fullName;
+
+  const heroContentContainer = document.querySelector(".hero-content");
+  heroContentContainer.insertAdjacentHTML(
+    "afterbegin",
+    parkInfoTemplate(parkData)
+  );
+
+  const heroImage = document.querySelector(".hero-img");
+  heroImage.src = parkData.images[0].url;
+
+  const intro = document.querySelector(".intro");
+  intro.insertAdjacentHTML(
+    "afterbegin",
+    `<h2>${data.fullName}</h2>
+    <p>${data.description}</p>`
+  )
+}
+
+function setCardInfo(data) {
+  const cardContainer = document.querySelector(".info");
+  const html = data.map(mediaCardTemplate);
+  cardContainer.insertAdjacentHTML("afterbegin", html.join(""));
+}
+
+function mediaCardTemplate(info) {
+  return `<div class="info-card">
+    <img src="${info.image}" alt="img">
+    <a href="${info.link}">${info.name}</a>
+    <p>${info.description}</p>
+    </div>`;
+}
 
 function parkInfoTemplate(info) {
-    return `<a href="/" class="hero-title">${info.name}</a>
+  return `<a href="/" class="hero-title">${info.name}</a>
     <div class="hero-info">
     <p>${info.designation}</p>
     <p>${info.states}</p>
     </div>`;
-
 }
-const heroContentContainer = document.querySelector(".hero-content")
-heroContentContainer.insertAdjacentHTML("afterbegin", parkInfoTemplate(parkData))
 
-const heroImage = document.querySelector(".hero-img")
-heroImage.src = parkData.images[0].url
+setHeaderInfo(parkData);
+setCardInfo(parkInfoLinksData);
