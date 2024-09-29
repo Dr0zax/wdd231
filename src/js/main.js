916bb64cc336d2007d7873405e7371123f350c44
Dr0zax <geodashfan4@gmai.com>
2024-09-28 18:50:28 -0600

    NPS part 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,27 @@
 import { park, parkInfoLinks } from "./parkService.mjs";
+import { setHeaderFooter } from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 
 const parkData = park;
-const parkInfoLinksData = parkInfoLinks;
 
 const parkTitle = document.querySelector(".title");
 parkTitle.innerHTML = parkData.fullName;
 
-function setHeaderInfo(data) {
-  const disclaimer = document.querySelector(".disclaimer-link");
-  disclaimer.href = parkData.url;
-  disclaimer.innerHTML = parkData.fullName;
-
-  const heroContentContainer = document.querySelector(".hero-content");
-  heroContentContainer.insertAdjacentHTML(
-    "afterbegin",
-    parkInfoTemplate(parkData)
-  );
-
-  const heroImage = document.querySelector(".hero-img");
-  heroImage.src = parkData.images[0].url;
-
+function setParkIntro(data) {
   const intro = document.querySelector(".intro");
   intro.insertAdjacentHTML(
     "afterbegin",
     `<h2>${data.fullName}</h2>
-    <p>${data.description}</p>`
-  )
+      <p>${data.description}</p>`
+  );
 }
 
-function setCardInfo(data) {
+function setParkInfoLinks(data) {
   const cardContainer = document.querySelector(".info");
   const html = data.map(mediaCardTemplate);
   cardContainer.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
-function mediaCardTemplate(info) {
-  return `<div class="info-card">
-    <img src="${info.image}" alt="img">
-    <a href="${info.link}">${info.name}</a>
-    <p>${info.description}</p>
-    </div>`;
-}
-
-function parkInfoTemplate(info) {
-  return `<a href="/" class="hero-title">${info.name}</a>
-    <div class="hero-info">
-    <p>${info.designation}</p>
-    <p>${info.states}</p>
-    </div>`;
-}
-
-setHeaderInfo(parkData);
-setCardInfo(parkInfoLinksData);
+setHeaderFooter(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks)
